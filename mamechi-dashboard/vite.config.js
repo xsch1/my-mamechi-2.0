522df3446edef04e1c455ddaebac42467554c886
xxschi <141630459+xxschi@users.noreply.github.com>
2025-08-12 16:52:59 +0900

    chore: set vite base for gh-pages

--- mamechi-dashboard/vite.config.js
+++ mamechi-dashboard/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/my-mamechi-2.0/', // 여기에 실제 저장소 이름 입력
+  build: {
+    outDir: 'dist',
+  },
+});
